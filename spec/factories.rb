04bbd792080393c28c8ea50536aabfc4b151a634
Lawrence Huang <lawrence.y.huang@gmail.com>
2012-10-30 19:09:23 +0800

    finished signup and enable ssl in production

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do 
+	factory :user do
+		name										"Lawrence Huang"
+		email										"mr.godfather@gmail.com"
+		password								"welcome"
+		password_confirmation 	"welcome"
+	end	
+end
